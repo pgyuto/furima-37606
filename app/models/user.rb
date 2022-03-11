9012320e5ad0165888eb10e6b1652b4e504d75c0
pgyuto <pgyuto0235@gmail.com>
2022-03-11 16:01:54 +0900

    ユーザモデルバリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z}
+  validates :first_name_kana, presence: true, format: { with: /\p{katakana}/}
+  validates :last_name_kana, presence: true, format: { with: /\p{katakana}/}
+  validates :birthday, presence: true
 end

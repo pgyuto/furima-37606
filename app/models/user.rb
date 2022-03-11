405c013c05a149159f9e64cb4c283885b344f596
pgyuto <pgyuto0235@gmail.com>
2022-03-11 18:13:32 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}, presence: true
+  validates :password,format: { with: /\A[a-zA-Z0-9]+\z/}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name_kana, presence: true, format: { with: /\p{katakana}/}

fcaae2f555f9513c8dca18848ae750ac8444032b
pgyuto <pgyuto0235@gmail.com>
2022-03-14 17:03:33 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,9 @@ class Item < ApplicationRecord
   belongs_to :category, :condition, :postage, :area, :post_date
   has_one_attached :image
 
-  validates :name, :text, :price, presence: true
-  validates :price, format: {with: /\A\d+\z/}, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+  validates :name, :text, :price, :image, presence: true
+  validates :price, format: {with: /\A\[0-9]+\z/, message: "is invalid. Input half-width characters"}
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range"}
   validates :category_id, :condition_id, :postage_id, :area_id, :post_date_id,
     numericality: { other_than: 1 , message: "can't be blank"}
 end

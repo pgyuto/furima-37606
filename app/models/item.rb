a0f2925e4b8f5e2558e07a7a7cdbcbc7f69e155e
pgyuto <pgyuto0235@gmail.com>
2022-03-14 18:50:46 +0900

    バリデーション修正（price)

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to :user
 
   validates :name, :text, :price, :image, presence: true
-  validates :price, format: {with: /\A\[0-9]+\z/, message: "is invalid. Input half-width characters"}
+  validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}
   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range"}
   validates :category_id, :condition_id, :postage_id, :area_id, :post_date_id,
     numericality: { other_than: 1 , message: "can't be blank"}

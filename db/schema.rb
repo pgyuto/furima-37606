e8597b0bb156823b4203c57858f711825045b46b
pgyuto <pgyuto0235@gmail.com>
2022-03-14 12:22:21 +0900

    カラム名修正

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2022_03_14_015847) do
     t.integer "condition_id", null: false
     t.integer "postage_id", null: false
     t.integer "area_id", null: false
-    t.integer "post_data_id", null: false
+    t.integer "post_date_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false

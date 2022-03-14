e73d478624c5b51216884bc2902719f056e9cf2d
pgyuto <pgyuto0235@gmail.com>
2022-03-14 12:23:17 +0900

    active_hashモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :postage, :area, :post_date
+
+  validates :name, :text, :price, presence: true
+  validates :category_id, :condition_id, :postage_id, :area_id, :post_date_id,
+    numericality: { other_than: 1 , message: "can't be blank"}
 end

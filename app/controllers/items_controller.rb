98a9d59eacba740ed7775e30de4e9f00bbb3a374
pgyuto <pgyuto0235@gmail.com>
2022-03-15 16:32:18 +0900

    ユーザー状態による遷移先の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -41,5 +42,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :name, :text, :category_id, :condition_id, :postage_id, :area_id, :post_date_id, :price, :image
     ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless current_user.id == Item.find(params[:id]).user.id
+      redirect_to action: :index
+    end
   end
 end

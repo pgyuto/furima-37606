89f10c68d021471ebdd0a65e55a425639f2a9597
pgyuto <pgyuto0235@gmail.com>
2022-03-18 16:31:59 +0900

    ビューの表示

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,2 +1,25 @@
 class BuysController < ApplicationController
+  def index
+    @buy_order = BuyOrder.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @buy_order = BuyOrder.new(buy_params)
+    @item = Item.find(params[:item_id])
+    if @buy_order.valid?
+      @buy_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  
+  def buy_params
+    params.require(:buy_order).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id]
+    )
+  end
 end
